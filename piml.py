1a27a37e07174a8deb8a130a8fb818c49e023640
pnroy <pnroy@uwaterloo.ca>
2020-10-18 15:47:54 -0400

    added sampling function

--- piml.py
+++ piml.py
@@ -1,14 +1,15 @@
 from argparse import ArgumentParser
 import os.path
 from os import path
+from rotor_pi import linrotstep
 import numpy as np
 import scipy as sp
 import subprocess
 
-
+RZERO=1e-10
 T=1. # temperature in Kelvin
 beta=1./T # in K^-1
-P=8 # number of beads
+P=16 # number of beads
 tau=beta/float(P)
 kB= 0.6950356  # 1/cm per Kelvin
 B=1. # rotational constant in Kelvin
@@ -19,10 +20,11 @@ Ngrid=1500
 delta_gamma=2./float(Ngrid)
 
 nskip=1
+nskip2=1000
 
 MC_steps=10000
-step_z=.3
-step_phi=.5
+step_z=.5
+step_phi=2.
 linprop_command='./linear_prop/linden.x'
 
 if (path.exists(linprop_command)==False):
@@ -54,6 +56,40 @@ path_angles=np.zeros((2,P),float)
 path_xyz=np.zeros((3,P),float)
 path_xyz_new=np.zeros((3,P),float)
 
+#create an off-diagonal rho
+Nz=30
+Nphi=30
+dz=2./float(Nz)
+dphi=2.*np.pi/float(Nphi)
+
+Ngrid_zphi=Nz*Nphi
+rho_eep=np.zeros((Ngrid_zphi,Ngrid_zphi),float)
+rho_E_eep=np.zeros((Ngrid_zphi,Ngrid_zphi),float)
+for i in range(Nz):
+	z=-1.+dz*float(i)
+	sint = np.sqrt(1.0 - z*z)
+	for j in range(Nphi):
+		phi=dphi*float(j)
+		x=sint*np.cos(phi)
+		y=sint*np.sin(phi)
+		for ip in range(Nz):
+			zp=-1.+dz*float(ip)
+			sintp = np.sqrt(1.0 - zp*zp)
+			for jp in range(Nphi):
+				phip=dphi*float(jp)
+				xp=sintp*np.cos(phip)
+				yp=sintp*np.sin(phip)
+				dot=x*xp+y*yp+z*zp
+				index=int((dot+1.)/delta_gamma)
+				if (index>=Ngrid):
+					index=Ngrid-1
+				rho_eep[i*Nphi+j,ip*Nphi+jp]=rho[index]
+
+print('building rho_eep done')
+
+
+Ctau=np.zeros
+
 N_accept=0
 N_total=0
 N_averages=0
@@ -74,103 +110,26 @@ for step in range(MC_steps):
 	#sample one bead at a time; can one improve this?
 	for p in P_list:
 
-		z=path_angles[0,p]
-		phi=path_angles[1,p]
-
-		#uniform MC move
-		z+=step_z*2.*(np.random.random()-.5)
-		phi+=step_phi*2.*np.pi*(np.random.random())
-
-		if (z >  1.0):
-			z = 2.0 - z
-		if (z < -1.0):
-			z = -2.0 - z
-		sint = np.sqrt(1.0 - z*z)
-
-		x=sint*np.cos(phi)
-		y=sint*np.sin(phi)
-
-		path_xyz_new[0,p] = x
-		path_xyz_new[1,p] = y
-		path_xyz_new[2,p] = z
-
-# the old density
-
-		dot0 = 0.0	
-		dot1 = 0.0
-		p_minus=p-1
-		p_plus=p+1
-		if (p_minus<0):
-			p_minus+=P
-		if (p_plus>=P):
-			p_plus-=P
-		for id in range(3):
-			dot0 += (path_xyz[id,p_minus]*path_xyz[id,p])
-			dot1 += (path_xyz[id,p]*path_xyz[id,p_plus])
-
-		# find points on cos_gamma grid
-		
-		index_0=int((dot0+1.)/delta_gamma)
-		index_1=int((dot1+1.)/delta_gamma)
-
-		dens_old = rho[index_0]*rho[index_1]
-
-		# potential
-#
-		pot_old=mu*path_xyz[2,p]
-
-# the new density 
-
-		dot0 = 0.0	
-		dot1 = 0.0
-		for id in range(3):
-			dot0 += (path_xyz[id,p_minus]*path_xyz_new[id,p])
-			dot1 += (path_xyz_new[id,p]*path_xyz[id,p_plus])
-		index_0=int((dot0+1.)/delta_gamma)
-		index_1=int((dot1+1.)/delta_gamma)
-		dens_new=rho[index_0]*rho[index_1]
-
-		rd=dens_new/dens_old
-
-		pot_new=mu*path_xyz_new[2,p]
-		rd *= np.exp(- tau*(pot_new-pot_old));
-   
-		Accepted = False
-
-		if (rd>1.0):
-			Accepted = True;
-		elif (rd>np.random.random()): 
-			Accepted = True;
-
-		N_total+=1
-
-		if (Accepted==True):
-			N_accept+=1
-			path_angles[0,p] = z
-			path_angles[1,p] = phi
-
-			for id in range(3):
-				path_xyz[id,p]=path_xyz_new[id,p]
-
+		N_total,N_accept=linrotstep(P,path_angles,step_z,step_phi,path_xyz,path_xyz_new,delta_gamma,rho,mu,tau,N_total,N_accept,p,RZERO)
+	
 	if (step%nskip==0):
 		#estimators
 		N_averages+=1
 		srot=0.
 		srot2=0.
 		for p in range(P):
-
 			dot1 = 0.0
 			p_plus=p+1
 			if (p_plus>=P):
 				p_plus-=P
 			for id in range(3):
 				dot1 += (path_xyz[id,p]*path_xyz[id,p_plus])
-
 			index_1=int((dot1+1.)/delta_gamma)
 			dens=rho[index_1]
+
 			E=rho_E[index_1]/dens
 			E2=(rho_E[index_1]/dens)**2
-			if (dens > 1.e-9):
+			if (dens > RZERO):
 				srot +=E
 				srot2 +=E2
 			#print(dot1,E)
@@ -178,6 +137,7 @@ for step in range(MC_steps):
 		E_avg+=srot
 		E2_avg+=srot2
 # output paths to a file
+	if (step%nskip2==0):
 		for p in range(P):
 			paths_output.write(str(path_xyz[0,p])+' '+str(path_xyz[1,p])+' '+str(path_xyz[2,p])+'\n')
 		paths_output.write('\n')
