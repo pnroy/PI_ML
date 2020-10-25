dbf67cb6567b76ab3b1b93f225a60b14919499dc
pnroy <pnroy@uwaterloo.ca>
2020-10-25 19:12:21 -0400

    bug fixes

--- piml.py
+++ piml.py
@@ -1,7 +1,7 @@
 from argparse import ArgumentParser
 import os.path
 from os import path
-from rotor_pi import linrotstep
+from rotor_pi import linrotstep,table_sample
 import numpy as np
 import scipy as sp
 import subprocess
@@ -9,22 +9,22 @@ import subprocess
 RZERO=1e-10
 T=1. # temperature in Kelvin
 beta=1./T # in K^-1
-P=16 # number of beads
+P=8 # number of beads
 tau=beta/float(P)
 kB= 0.6950356  # 1/cm per Kelvin
 B=1. # rotational constant in Kelvin
 B*=kB # # rotational constant in 1/CM (required for linden.x)
 mu=0.
 Ngrid=1500
+delta_gamma=2./float(Ngrid-1)
 
-delta_gamma=2./float(Ngrid)
+n_equilibrate=1000
+nskip=100
+nskip2=100
 
-nskip=1
-nskip2=1000
-
-MC_steps=10000
+MC_steps=100000
 step_z=.5
-step_phi=2.
+step_phi=.3
 linprop_command='./linear_prop/linden.x'
 
 if (path.exists(linprop_command)==False):
@@ -53,14 +53,28 @@ for line in density_file:
 
 # define a path
 path_angles=np.zeros((2,P),float)
+#initialize xyz coordinates
 path_xyz=np.zeros((3,P),float)
 path_xyz_new=np.zeros((3,P),float)
+for p in range(P):
+	z=path_angles[0,p]
+	phi=path_angles[1,p]
+	sint = np.sqrt(1.0 - z*z)
+	x=sint*np.cos(phi)
+	y=sint*np.sin(phi)
+	path_xyz[0,p] = x
+	path_xyz[1,p] = y
+	path_xyz[2,p] = z
+	path_xyz_new[0,p] = x
+	path_xyz_new[1,p] = y
+	path_xyz_new[2,p] = z
+
 
 #create an off-diagonal rho
 Nz=30
 Nphi=30
-dz=2./float(Nz)
-dphi=2.*np.pi/float(Nphi)
+dz=2./float(Nz-1)
+dphi=(2.*np.pi)/float(Nphi-1)
 
 Ngrid_zphi=Nz*Nphi
 rho_eep=np.zeros((Ngrid_zphi,Ngrid_zphi),float)
@@ -69,14 +83,14 @@ for i in range(Nz):
 	z=-1.+dz*float(i)
 	sint = np.sqrt(1.0 - z*z)
 	for j in range(Nphi):
-		phi=dphi*float(j)
+		phi=-np.pi+dphi*float(j)
 		x=sint*np.cos(phi)
 		y=sint*np.sin(phi)
 		for ip in range(Nz):
 			zp=-1.+dz*float(ip)
 			sintp = np.sqrt(1.0 - zp*zp)
 			for jp in range(Nphi):
-				phip=dphi*float(jp)
+				phip=np.pi+dphi*float(jp)
 				xp=sintp*np.cos(phip)
 				yp=sintp*np.sin(phip)
 				dot=x*xp+y*yp+z*zp
@@ -84,11 +98,16 @@ for i in range(Nz):
 				if (index>=Ngrid):
 					index=Ngrid-1
 				rho_eep[i*Nphi+j,ip*Nphi+jp]=rho[index]
+#				rho_eep[ip*Nphi+jp,i*Nphi+j]=rho_eep[i*Nphi+j,ip*Nphi+jp]
 
+# for i in range(Nz):
+# 	for j in range(Nphi):
+# 		for ip in range(i,Nz):
+# 			for jp in range(j,Nphi):
+# 				rho_eep[ip*Nphi+jp,i*Nphi+j]=rho_eep[i*Nphi+j,ip*Nphi+jp]
 print('building rho_eep done')
 
-
-Ctau=np.zeros
+#Ctau=np.zeros
 
 N_accept=0
 N_total=0
@@ -98,6 +117,11 @@ E_avg = 0.0
 E2_avg = 0.0
 
 paths_output=open('paths.xyz','w')
+
+histo_out=open('histo.xyz','w')
+nbins=20
+dx=2./float(nbins)
+xyz_histo=np.zeros((3,nbins),float)
 
 P_list=[]
 for p in range(0,P,2):
@@ -110,9 +134,10 @@ for step in range(MC_steps):
 	#sample one bead at a time; can one improve this?
 	for p in P_list:
 
-		N_total,N_accept=linrotstep(P,path_angles,step_z,step_phi,path_xyz,path_xyz_new,delta_gamma,rho,mu,tau,N_total,N_accept,p,RZERO)
-	
-	if (step%nskip==0):
+		N_total,N_accept=linrotstep(P,path_angles,step_z,step_phi,path_xyz,path_xyz_new,delta_gamma,rho,Ngrid,mu,tau,N_total,N_accept,p,RZERO)
+#		N_total,N_accept=table_sample(P,path_angles,step_z,step_phi,path_xyz,path_xyz_new,delta_gamma,rho,Ngrid,rho_eep,dz,dphi,Nz,Nphi,mu,tau,N_total,N_accept,p,RZERO)
+
+	if (step%nskip==0 and step >n_equilibrate):
 		#estimators
 		N_averages+=1
 		srot=0.
@@ -125,6 +150,10 @@ for step in range(MC_steps):
 			for id in range(3):
 				dot1 += (path_xyz[id,p]*path_xyz[id,p_plus])
 			index_1=int((dot1+1.)/delta_gamma)
+
+			if (index_1>=Ngrid):
+				index_1=Ngrid-1
+
 			dens=rho[index_1]
 
 			E=rho_E[index_1]/dens
@@ -133,19 +162,32 @@ for step in range(MC_steps):
 				srot +=E
 				srot2 +=E2
 			#print(dot1,E)
+			for i in range(3):
+				index=int((path_xyz[i,p]+1.)/dx)
+				if (index>=nbins):
+					index=nbins-1
+				xyz_histo[i,index]+=1.
 
 		E_avg+=srot
 		E2_avg+=srot2
 # output paths to a file
 	if (step%nskip2==0):
 		for p in range(P):
-			paths_output.write(str(path_xyz[0,p])+' '+str(path_xyz[1,p])+' '+str(path_xyz[2,p])+'\n')
+			paths_output.write(str(path_xyz[0,p])+' '+str(path_xyz[1,p])+' '+str(path_xyz[2,p])+' '+str(path_angles[0,p])+' '+str(path_angles[1,p])+'\n')
+			
 		paths_output.write('\n')
 paths_output.close()
+
+for index in range(nbins):
+	histo_out.write(str(-1.+float(index)*dx)+' ')	
+	for i in range(3):
+		histo_out.write(str(xyz_histo[i,index]/float(N_total)/dx)+' ')
+	histo_out.write('\n')
+histo_out.close()
 
 print('accept ratio: ',float(N_accept)/float(N_total))
 E_avg=E_avg/float(N_averages)
 E2_avg=E2_avg/float(N_averages)
-print('<K>: ',E_avg,np.sqrt((E2_avg-E_avg**2))/(float(N_averages)))
+print('<E> (in K): ',E_avg,np.sqrt((E2_avg-E_avg**2))/(float(N_averages)))
 
 
--- rotor_pi.py
+++ rotor_pi.py
@@ -1,5 +1,5 @@
 import numpy as np
-def linrotstep(P,path_angles,step_z,step_phi,path_xyz,path_xyz_new,delta_gamma,rho,mu,tau,N_total,N_accept,p,RZERO):
+def linrotstep(P,path_angles,step_z,step_phi,path_xyz,path_xyz_new,delta_gamma,rho,Ngrid,mu,tau,N_total,N_accept,p,RZERO):
    p_minus=p-1
    p_plus=p+1
    if (p_minus<0):
@@ -19,9 +19,11 @@ def linrotstep(P,path_angles,step_z,step_phi,path_xyz,path_xyz_new,delta_gamma,r
    if (z < -1.0):
       z = -2.0 - z
    sint = np.sqrt(1.0 - z*z)
-
    x=sint*np.cos(phi)
    y=sint*np.sin(phi)
+
+# adjust pji in -Pi..Pi range
+#   phi=(np.arctan2(y,x))
 
    path_xyz_new[0,p] = x
    path_xyz_new[1,p] = y
@@ -37,6 +39,13 @@ def linrotstep(P,path_angles,step_z,step_phi,path_xyz,path_xyz_new,delta_gamma,r
    # find points on cos_gamma grid  
    index_0=int((dot0+1.)/delta_gamma)
    index_1=int((dot1+1.)/delta_gamma)
+
+   if (index_0>=Ngrid):
+      print(index_0)
+      index_0=Ngrid-1
+   if (index_1>=Ngrid):
+      print(index_1)
+      index_1=Ngrid-1
 
    dens_old = rho[index_0]*rho[index_1]
 
@@ -55,6 +64,242 @@ def linrotstep(P,path_angles,step_z,step_phi,path_xyz,path_xyz_new,delta_gamma,r
       dot1 += (path_xyz_new[id,p]*path_xyz[id,p_plus])
    index_0=int((dot0+1.)/delta_gamma)
    index_1=int((dot1+1.)/delta_gamma)
+
+   if (index_0>=Ngrid):
+      print(index_0)
+      index_0=Ngrid-1
+   if (index_1>=Ngrid):
+      print(index_1)
+      index_1=Ngrid-1
+
+   dens_new=rho[index_0]*rho[index_1]
+
+   if (dens_new<RZERO): 
+      dens_new = 0.0
+   pot_new=mu*path_xyz_new[2,p]
+
+
+   if (dens_old>RZERO):
+      rd = dens_new/dens_old
+   else: 
+      rd = 1.0
+
+   rd *= np.exp(- tau*(pot_new-pot_old));
+
+   Accepted = False
+
+   if (rd>1.0):
+      Accepted = True
+   elif (rd>np.random.random()): 
+      Accepted = True
+
+   N_total+=1
+
+   if (Accepted==True):
+      N_accept+=1
+      path_angles[0,p] = z
+      path_angles[1,p] = phi
+
+      for id in range(3):
+         path_xyz[id,p]=path_xyz_new[id,p]
+   return N_total,N_accept
+   
+def table_sample(P,path_angles,step_z,step_phi,path_xyz,path_xyz_new,delta_gamma,rho,Ngrid,rho_eep,dz,dphi,Nz,Nphi,mu,tau,N_total,N_accept,p,RZERO):
+   p_minus=p-1
+   p_plus=p+1
+   if (p_minus<0):
+      p_minus+=P
+   if (p_plus>=P):
+      p_plus-=P
+
+# the old density
+
+   i0=int((path_angles[0,p_minus]+1.)/dz)
+   j0=int(path_angles[1,p_minus]/dphi+np.pi)
+   i1=int((path_angles[0,p]+1.)/dz)
+   j1=int(path_angles[1,p]/dphi+np.pi)
+   i2=int((path_angles[0,p_plus]+1.)/dz)
+   j2=int(path_angles[1,p_plus]/dphi+np.pi)
+
+   if (i0>=Nz):
+      i0=Nz-1
+   if (i1>=Nz):
+      i1=Nz-1
+   if (i2>=Nz):
+      i2=Nz-1
+
+   if (j0>=Nphi):
+      j0=Nphi-1
+   if (j1>=Nphi):
+      j1=Nphi-1
+   if (j2>=Nphi):
+      j2=Nphi-1
+
+   dens_old = rho_eep[i0*Nphi+j0,i1*Nphi+j1]*rho_eep[i1*Nphi+j1,i2*Nphi+j2]
+
+# test below
+   # dot0 = 0.0     
+   # dot1 = 0.0
+   # for id in range(3):
+   #    dot0 += (path_xyz[id,p_minus]*path_xyz[id,p])
+   #    dot1 += (path_xyz[id,p]*path_xyz[id,p_plus])
+
+   # # find points on cos_gamma grid  
+   # index_0=int((dot0+1.)/delta_gamma)
+   # index_1=int((dot1+1.)/delta_gamma)
+
+   # if (index_0>=Ngrid):
+   #    index_0=Ngrid-1
+   # if (index_1>=Ngrid):
+   #    index_1=Ngrid-1
+
+   # dens_old2 = rho[index_0]*rho[index_1]
+
+   # print(rho_eep[i0*Nphi+j0,i1*Nphi+j1],rho[index_0])
+
+   if (dens_old<RZERO): 
+      dens_old = 0.0
+
+   # potential
+   pot_old=mu*path_xyz[2,p]
+
+   z=path_angles[0,p]
+   phi=path_angles[1,p]
+   #uniform MC move
+   z+=step_z*(np.random.random()-.5)
+   phi+=step_phi*(np.random.random()-.5)
+
+   if (z >  1.0):
+      z = 2.0 - z
+   if (z < -1.0):
+      z = -2.0 - z
+
+   sint = np.sqrt(1.0 - z*z)
+   x=sint*np.cos(phi)
+   y=sint*np.sin(phi)
+
+   # adjust pji in -Pi..Pi range
+   phi=(np.arctan2(y,x))
+
+   i1=int((z+1.)/dz)
+   j1=int(phi/dphi+np.pi)
+
+   if (i1>=Nz):
+      i1=Nz-1
+   if (j1>=Nphi):
+      j1=Nphi-1
+
+   path_xyz_new[0,p] = x
+   path_xyz_new[1,p] = y
+   path_xyz_new[2,p] = z
+
+# the new density 
+
+   dens_new=rho_eep[i0*Nphi+j0,i1*Nphi+j1]*rho_eep[i1*Nphi+j1,i2*Nphi+j2]
+
+
+   if (dens_new<RZERO): 
+      dens_new = 0.0
+   pot_new=mu*path_xyz_new[2,p]
+
+   if (dens_old>RZERO):
+      rd = dens_new/dens_old
+   else: 
+      rd = 1.0
+
+   rd *= np.exp(- tau*(pot_new-pot_old));
+
+   Accepted = False
+
+   if (rd>1.0):
+      Accepted = True
+   elif (rd>np.random.random()): 
+      Accepted = True
+
+   N_total+=1
+
+   if (Accepted==True):
+      N_accept+=1
+      path_angles[0,p] = z
+      path_angles[1,p] = phi
+
+      for id in range(3):
+         path_xyz[id,p]=path_xyz_new[id,p]
+   return N_total,N_accept
+def cosine_sample(P,path_angles,step_z,step_phi,path_xyz,path_xyz_new,delta_gamma,rho,Ngrid,mu,tau,N_total,N_accept,p,RZERO):
+# under development
+   p_minus=p-1
+   p_plus=p+1
+   if (p_minus<0):
+      p_minus+=P
+   if (p_plus>=P):
+      p_plus-=P
+
+   z=path_angles[0,p]
+   phi=path_angles[1,p]
+
+   #uniform MC move
+   z+=step_z*(np.random.random()-.5)
+   phi+=step_phi*(np.random.random()-.5)
+
+   if (z >  1.0):
+      z = 2.0 - z
+   if (z < -1.0):
+      z = -2.0 - z
+   sint = np.sqrt(1.0 - z*z)
+   x=sint*np.cos(phi)
+   y=sint*np.sin(phi)
+
+# adjust pji in -Pi..Pi range
+#   phi=(np.arctan2(y,x))
+
+   path_xyz_new[0,p] = x
+   path_xyz_new[1,p] = y
+   path_xyz_new[2,p] = z
+
+# the old density
+   dot0 = 0.0  
+   dot1 = 0.0
+   for id in range(3):
+      dot0 += (path_xyz[id,p_minus]*path_xyz[id,p])
+      dot1 += (path_xyz[id,p]*path_xyz[id,p_plus])
+
+   # find points on cos_gamma grid  
+   index_0=int((dot0+1.)/delta_gamma)
+   index_1=int((dot1+1.)/delta_gamma)
+
+   if (index_0>=Ngrid):
+      print(index_0)
+      index_0=Ngrid-1
+   if (index_1>=Ngrid):
+      print(index_1)
+      index_1=Ngrid-1
+
+   dens_old = rho[index_0]*rho[index_1]
+
+   if (dens_old<RZERO): 
+      dens_old = 0.0
+
+   # potential
+   pot_old=mu*path_xyz[2,p]
+
+# the new density 
+
+   dot0 = 0.0  
+   dot1 = 0.0
+   for id in range(3):
+      dot0 += (path_xyz[id,p_minus]*path_xyz_new[id,p])
+      dot1 += (path_xyz_new[id,p]*path_xyz[id,p_plus])
+   index_0=int((dot0+1.)/delta_gamma)
+   index_1=int((dot1+1.)/delta_gamma)
+
+   if (index_0>=Ngrid):
+      print(index_0)
+      index_0=Ngrid-1
+   if (index_1>=Ngrid):
+      print(index_1)
+      index_1=Ngrid-1
+
    dens_new=rho[index_0]*rho[index_1]
 
    if (dens_new<RZERO): 
